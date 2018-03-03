ace04fb64c2e1e7f38fa33b5949ddb3e5f28f23e
Daniel <daniel.hornung@gmx.de>
2018-03-03 14:22:52 +0100

    WIP: scripts/install_on_uberspace.sh: More or less reflects my state
    Does not run yet.

--- scripts/install_on_uberspace.sh
+++ scripts/install_on_uberspace.sh
@@ -34,15 +34,15 @@ grep -Fxq "${EXPORT_LINE}" ~/.bash_profile || echo "$EXPORT_LINE" >> ~/.bash_pro
 echo "gem: --user-install --no-rdoc --no-ri" > ~/.gemrc
 echo "RUBYVERSION=${RUBYVERSION}" > ~/etc/rubyversion
 
+source ~/.bash_profile
+
+
+##################
+## Get OpenProject
 
 rm -r openproject
 git clone https://github.com/opf/openproject-ce.git --branch stable/7 --depth 1 openproject || die
 cd openproject || die
-
-
-
-# echo "export SECRET_KEY_BASE=$(./bin/rake secret)" >> ~/.bash_profile
-source ~/.bash_profile
 
 gem install bundler || die
 bundle install --deployment --without postgres sqlite development test therubyracer docker || die
@@ -82,14 +82,76 @@ RAILS_ENV="production" ./bin/rake db:migrate || die
 RAILS_ENV="production" LOCALE="$OP_LOCALE" ./bin/rake db:seed || die
 RAILS_ENV="production" ./bin/rake assets:precompile || die
 
-# RAILS_ENV=production LOCALE=de bundle exec rake generate_secret_token db:create db:migrate db:seed
+######################
+## Create, save and load a new secret
+NEW_SECRET=$(./bin/rake secret)
 
-# npm install -g bower
-# npm install
-# RAILS_ENV=production bundle exec rake assets:precompile
+# Either replace or create from scratch
+if grep -q "^export SECRET_KEY_BASE=.*" ~/.bash_profile ; then
+    sed -i -E 's/^(export SECRET_KEY_BASE=).*/\1'"$NEW_SECRET"'/g' ~/.bash_profile
+else
+    echo "export SECRET_KEY_BASE=${NEW_SECRET}" >> ~/.bash_profile
+fi
+source ~/.bash_profile
+
+#############
+## Install assets
+
+# Is this really necessary?
+cp -r public/* ~/html/
 
 
-rm -r ~/html/assets/ || die
-cp -r public/* ~/html/ || die
+###############################
+## Create and start services
+
+test -d ~/service || uberspace-setup-svscan
+
+# create openproject-web
+cat <<__EOF__ > ~/bin/openproject-web
+#!/bin/sh
+# This is needed to find gems installed with --user-install
+export HOME=$HOME
+source \$HOME/.bash_profile
+# Get into the project directory and start the Rails server
+cd \$HOME/apps/openproject
+exec bundle exec unicorn --port ${PORT} --env production
+__EOF__
+chmod +x ~/bin/openproject-web
+# create openproject-worker
+cat <<__EOF__ > ~/bin/openproject-worker
+#!/bin/sh
+# This is needed to find gems installed with --user-install
+export HOME=$HOME
+source \$HOME/.bash_profile
+# we're faster and use the right database in production
+export RAILS_ENV=production
+# Get into the project directory and start the Rails server
+cd \$HOME/apps/openproject
+exec bundle exec rake jobs:work
+__EOF__
+chmod +x ~/bin/openproject-worker
+
+# Setup and start services
+uberspace-setup-service openproject-web ~/bin/openproject-web
+uberspace-setup-service openproject-worker ~/bin/openproject-worker
+
+#################################
+## We should be finished now
+
+echo <<__EOF__
+
+Congratulations!  OpenProject should be running now on your machine.
+
+You should be able to access it in a web browser at
+
+https://${USER}.$(hostname)
+
+Logs are in $PWD/log/production.log.
+
+For restarting the services later, type:
+
 svc -du ~/service/openproject-web
 svc -du ~/service/openproject-worker
+
+__EOF__
+
